e04f334d524cf0576c3ac01e77664aaa19da97f6
ajithkumar110302 <ajithkumar110302@gmail.com>
2025-11-23 22:45:59 +0530

    Updated resume

--- components/FileDownloadButton.tsx
+++ components/FileDownloadButton.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const FileDownloadButton = () => {
   return (
-    <a href="/Ajithkumar_A_Software_Engineer_2023.pdf" download>
+    <a href="/Ajithkumar_A_Resume.pdf" download>
         <button className="border text-sm font-medium relative border-neutral-200 dark:border-white/[0.2] text-black dark:text-white px-4 py-2 rounded-full">
           <span>Resume</span>
           <span className="absolute inset-x-0 w-1/2 mx-auto -bottom-px bg-gradient-to-r from-transparent via-blue-500 to-transparent  h-px" />
